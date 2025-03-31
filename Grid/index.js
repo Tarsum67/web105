479629ecc3bf620c9b98746e462ed7af97e6feeb
Travis Routhier <143567066+Tarsum67@users.noreply.github.com>
2025-03-31 09:13:48 -0500

    Add files via upload

---
+++ Grid/index.js
@@ -0,0 +1,26 @@
+const mainImg = document.querySelector("#main-img img");
+
+function change01() {
+  mainImg.src = "imgs/elden ring BG.jpg";
+  mainImg.alt = "elden ring";
+}
+
+function change02() {
+  mainImg.src = "imgs/elden-ring-shadow-of-erdtree-dlc.jpg";
+  mainImg.alt = "elden-ring-shadow-of-erdtree";
+}
+
+function change03() {
+  mainImg.src = "imgs/JuicePurple.jpg";
+  mainImg.alt = "JuicePurple";
+}
+
+function change04() {
+  mainImg.src = "imgs/elden ring BG.jpg";
+  mainImg.alt = "elden ring BG";
+}
+
+function change05() {
+  mainImg.src = "imgs/SpawnMoon.jpg";
+  mainImg.alt = "SpawnMoon";
+}
